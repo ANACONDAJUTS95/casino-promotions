17936c8a540f1ec59bdf01201e2536218058a64f
ANACONDAJUTS <59391646+ANACONDAJUTS95@users.noreply.github.com>
2025-10-24 17:12:44 +0800

    Initial Designs

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,34 @@
 import Image from "next/image";
+import { FaAngleDown, FaGear, FaLocationDot } from "react-icons/fa6";
+import { HiMiniSparkles } from "react-icons/hi2";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col min-h-screen items-center justify-center font-sans bg-gray-100">
+      <div className="fixed flex flex-row gap-6 items-center justify-between top-10 right-10">
+        <div className="flex flex-row gap-2 items-center bg-black rounded-md py-2 px-4">
+          <FaLocationDot className="text-gray-100 text-xl" />
+          <h1 className="text-gray-100">Location</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="bg-black rounded-md p-2.5">
+          <FaGear className="text-gray-100 text-xl" />
         </div>
-      </main>
+      </div>
+      <h1 className="text-4xl text-black font-bold">GAMBIT</h1>
+      <h2 className="text-gray-500">
+        Gaming Analysis & Machine-Based Intelligence Tracker
+      </h2>
+
+      <div className="mt-6 flex flex-row w-1/4 px-4 items-center justify-between">
+        <button className="bg-black text-white px-4 py-2 rounded-md">
+          Run Search
+        </button>
+        <div className="flex flex-row gap-2 items-center">
+          <HiMiniSparkles className="text-black" />
+          <h1 className="text-black text-sm">Gemini 2.5-Flash</h1>
+          <FaAngleDown className="text-black text-sm" />
+        </div>
+      </div>
     </div>
   );
 }
